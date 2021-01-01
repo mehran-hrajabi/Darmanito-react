9dca416c07ae855b05b232616e417cde0eb726e9
Mehran <mehran.hrajabi98@gmail.com>
2021-01-01 21:56:07 +0330

    Add missing files and js config for contact form

--- src/components/ContactPageForm/ContactPageForm.js
+++ src/components/ContactPageForm/ContactPageForm.js
@@ -1,32 +1,82 @@
 import React, {Component} from 'react';
+import Input from './ContactInput';
 import '../../assets/sass/components/ContactPageForm/_contactPageForm.scss';
 
 class ContactForm extends Component {
+
+    state = {
+        contactForm: {
+            name: {
+                elementType: "input",
+                elementConfig: {
+                    type: "text",
+                    placeholder: "نام و نام خانوادگی خود را وارد کنید"
+                },
+                value: "",
+                label: "نام و نام خانوادگی"
+            },
+
+            emailPhone: {
+                elementType: "input",
+                elementConfig: {
+                    type: "text",
+                    placeholder: "ایمیل یا شماره همراه خود را وارد کنید"
+                },
+                value: "",
+                label: "ایمیل یا شماره همراه"
+            },
+
+            message: {
+                elementType: "textarea",
+                elementConfig: {
+                    placeholder: "پیام خود را بنویسید"
+                },
+                value: "",
+                label: "پیام شما"
+            }
+        }            
+    }
+
+    inputChangedHandler = (event, inputID) => {
+        const updatedForm = {
+            ...this.state.contactForm
+        };
+        const updatedElement = {
+            ...updatedForm[inputID]
+        };
+        updatedElement.value = event.target.value;
+        updatedForm[inputID] = updatedElement;
+        this.setState({contactForm: updatedForm});
+    }
+
     render(){
+            
+        let formElementsArray = [];
+        for(let key in this.state.contactForm){
+            formElementsArray.push({
+                id: key,
+                config: this.state.contactForm[key]
+            })
+        }
+
+        let form = (
+            <form>
+                {formElementsArray.map(formElement => (
+                    <Input 
+                        key = {formElement.id}
+                        elementType = {formElement.config.elementType}
+                        elementConfig = {formElement.config.elementConfig}
+                        value = {formElement.config.value}
+                        label = {formElement.config.label}
+                        changed = {(event) => this.inputChangedHandler(event, formElement.id)} />
+                ))}
+                <button type="button">ارسال پیام</button>
+            </form>
+        );
+
+
         return(
-            <div className="contact-form">
-                <form>
-                    <div className="contact-form_element">
-                        <label for="name">نام و نام خانوادگی</label>
-                        <input  id="name" type="text"
-                        placeholder="نام و نام خانوادگی خود را وارد کنید" />
-                    </div>
-
-                    <div className="contact-form_element">
-                        <label for="email_phone">ایمیل یا شماره همراه</label>
-                        <input id="email_phone" type="text"
-                        placeholder="ایمیل یا شماره همراه خود را وارد کنید" />
-                    </div>
-
-                    <div className="contact-form_element">
-                        <label for="message">پیام شما</label>
-                        <textarea id="message" className="contact-form_element"
-                        placeholder="پیام خود را بنویسید" ></textarea>
-                    </div>
-
-                    <button type="button">ارسال پیام</button>
-                </form>
-            </div>
+            <div className="contact-form">{form}</div>
         );
     }
 }
