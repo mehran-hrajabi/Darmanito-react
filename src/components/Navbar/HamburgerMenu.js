d325cf1182c9def6c85c2f039119aec5253b3477
Mehran <mehran.hrajabi98@gmail.com>
2021-01-06 20:39:20 +0330

    Add hamburger menu to navigation bar

---
+++ src/components/Navbar/HamburgerMenu.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Logo from './Logo';
+import DownloadBtn from './DownloadBtn';
+
+import '../../assets/sass/components/Navbar/_navbar.scss';
+
+
+const hamburgerMenu = (props) =>{
+    return(
+        <div className="hamburger_menu">
+            <div className="menu_logo">
+                <Logo />
+            </div>
+
+            {props.children}
+
+            <div className="hamburger_body">
+                <a>صفحه اصلی</a>
+                <a>عضویت در داروخانه ها</a>
+                <a>درباره ما</a>
+                <a>تماس با ما</a>
+                <a>سوالات متداول</a>
+                <a>قوانین و شرایط</a>
+                <hr />
+                <DownloadBtn />
+            </div>
+        </div>
+    );
+}
+
+export default hamburgerMenu;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, {Component} from "react";
 import Logo from './Logo';
 import NavLinks from './NavLinks';
 import DownloadBtn from './DownloadBtn';
+import Hamburger from './HamburgerMenu';
+
+import Icon from '../../assets/img/menu_icon.svg';
+import Close from '../../assets/img/close_btn.svg';
 import '../../assets/sass/components/Navbar/_navbar.scss';
 
-const navbar = () => {
-    return (
-        <div class="main-nav">
-            <Logo />
-            <NavLinks />
-            <DownloadBtn />
-        </div>
-    );
+class navbar extends Component {
+    state = {
+        showMenu: false
+    }
+
+    hamburgerMenuHandler = () => {
+        let doesShow = this.state.showMenu;
+        this.setState({showMenu: !doesShow});
+        console.log(this.state.showMenu);
+    }
+    render(){
+        let hamburgerMenu = null;
+        if(this.state.showMenu){
+            hamburgerMenu = <Hamburger>
+                                <a onClick={this.hamburgerMenuHandler}><img src={Close} /></a>
+                            </Hamburger>
+        }
+
+        return (
+            <div className="main-nav">
+                <Logo />
+                <NavLinks />
+                <DownloadBtn />
+                <div className="hamburger_icon">
+                    <img src={Icon} onClick={this.hamburgerMenuHandler} />
+                </div>
+                {hamburgerMenu}
+            </div>
+        );
+    }
 }
 
 export default navbar;
