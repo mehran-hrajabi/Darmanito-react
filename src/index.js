380e62fcbec28fef217f856e0ad57783b57085de
Mehran <mehran.hrajabi98@gmail.com>
2021-01-14 04:38:10 +0330

    Add project assets to html index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+/*import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { Route, Link } from 'react-router-dom';*/
+import App from '../src/containers/App';
 import reportWebVitals from './reportWebVitals';
+/*import reducer from './store/reducer';*/
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/*const store = createStore(reducer);
+const app = (
+    <Provider store={store}>
+      <Form />
+    </Provider>
+)*/
+ReactDOM.render(<App />,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
