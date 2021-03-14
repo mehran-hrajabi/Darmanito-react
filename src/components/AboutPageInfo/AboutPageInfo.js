21a6f32f804b28a4288045fa7cdac50153e689ae
Mehran <mehran.hrajabi98@gmail.com>
2021-03-14 15:05:28 +0330

    Fix the other modal and input

--- src/components/AboutPageInfo/AboutPageInfo.js
+++ src/components/AboutPageInfo/AboutPageInfo.js
@@ -4,13 +4,10 @@ import Modal from '../UI/Modal/Modal';
 import '../../assets/sass/components/AboutPageInfo/_aboutPageInfo.scss';
 
 class AboutInfo extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            showModal: false,
-            isValid: false
-        };
-        this.modal = null;
+    state = {
+        showModal: false,
+        isValid: false,
+        inputText: ""
     }
 
     phoneValidation = (event) => {
@@ -21,32 +18,33 @@ class AboutInfo extends Component {
         else{
             this.setState({isValid: false});
         }
+        this.setState({
+            inputText: number
+        });
     }
+
     showModal = () => {
         if(this.state.isValid){
-            this.setState({showModal: true});
-            this.modal = (
-                <Modal show={this.state.showModal} title="ثبت نام با موفقیت انجام شد"
-                txt="با شما تماس خواهیم گرفت">
-                    <button onClick={this.closeModal}>برگشت</button>
-                </Modal>
-            );
+            this.setState({showModal: true, inputText: ""});
         }
     }
     closeModal = () => {
         this.setState({showModal: false, isValid: false});
-        this.modal = null;
     }
 
     render(){
         return(
             <div className="about-info">
-                {this.modal}
+                {this.state.showModal ? (
+                    <Modal backdropAction={this.closeModal} show={this.state.showModal} title="ارسال موفق" txt="لینک دانلود برای شما ارسال شد.">
+                        <button onClick={this.closeModal}>برگشت</button>
+                    </Modal> 
+                ) : null}
                 <h2>شما هم در سلامت و بهبود مردم کشورمون موثر باشید.</h2>
                 <p>با ثبت نام به عنوان داروخانه یا فروشگاه و داروخانه شما هم به سیستم درمان کمک کنید
                     و به هزاران کاربر خدمات ارائه بدین و روزانه درآمد سفارش های خودتون رو تسویه کنید. </p>
 
-                <RegistryInput clicked={this.showModal} isValid={this.state.isValid} validation={this.phoneValidation} />
+                <RegistryInput value={this.state.inputText} clicked={this.showModal} isValid={this.state.isValid} validation={this.phoneValidation} />
             </div>
         );        
     }
